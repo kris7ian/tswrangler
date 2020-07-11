e67388e4f4556f55a778a7edba1831c655439b13
Kristian <kr.arve@gmail.com>
2020-07-11 16:59:10 +0200

    adjusted repo url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small package to help wrangling pandas time series dataframes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/kris7ian/tswrangler",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
